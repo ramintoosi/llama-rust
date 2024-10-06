57f0a55a31fdd6863af79c5ddc42dc2682aaa5ee
Ramin <Ramin Toosi>
2024-10-06 19:28:51 +0330

    chat mode code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-//! This is a translation of simple.cpp in llama.cpp using llama-cpp-2.
 #![allow(
     clippy::cast_possible_wrap,
     clippy::cast_possible_truncation,
@@ -7,7 +6,7 @@
 )]
 
 use anyhow::{anyhow, bail, Context, Result};
-use clap::Parser;
+use clap::{arg, Parser, Subcommand};
 use hf_hub::api::sync::ApiBuilder;
 use llama_cpp_2::context::params::LlamaContextParams;
 use llama_cpp_2::ggml_time_us;
@@ -25,70 +24,15 @@ use std::path::PathBuf;
 use std::pin::pin;
 use std::str::FromStr;
 use std::time::Duration;
+use log::error;
 
-#[derive(clap::Parser, Debug, Clone)]
-struct Args {
-    /// The path to the model
-    #[command(subcommand)]
-    model: Model,
-    /// The prompt
-    #[clap(short = 'p', long)]
-    prompt: Option<String>,
-    /// Read the prompt from a file
-    #[clap(short = 'f', long, help = "prompt file to start generation")]
-    file: Option<String>,
-    /// set the length of the prompt + output in tokens
-    #[arg(long, default_value_t = 32)]
-    n_len: i32,
-    /// override some parameters of the model
-    #[arg(short = 'o', value_parser = parse_key_val)]
-    key_value_overrides: Vec<(String, ParamOverrideValue)>,
-    /// Disable offloading layers to the gpu
-    #[cfg(any(feature = "cuda", feature = "vulkan"))]
-    #[clap(long)]
-    disable_gpu: bool,
-    #[arg(short = 's', long, help = "RNG seed (default: 1234)")]
-    seed: Option<u32>,
-    #[arg(
-        short = 't',
-        long,
-        help = "number of threads to use during generation (default: use all available threads)"
-    )]
-    threads: Option<i32>,
-    #[arg(
-        long,
-        help = "number of threads to use during batch and prompt processing (default: use all available threads)"
-    )]
-    threads_batch: Option<i32>,
-    #[arg(
-        short = 'c',
-        long,
-        help = "size of the prompt context (default: loaded from themodel)"
-    )]
-    ctx_size: Option<NonZeroU32>,
-}
-
-/// Parse a single key-value pair
-fn parse_key_val(s: &str) -> Result<(String, ParamOverrideValue)> {
-    let pos = s
-        .find('=')
-        .ok_or_else(|| anyhow!("invalid KEY=value: no `=` found in `{}`", s))?;
-    let key = s[..pos].parse()?;
-    let value: String = s[pos + 1..].parse()?;
-    let value = i64::from_str(&value)
-        .map(ParamOverrideValue::Int)
-        .or_else(|_| f64::from_str(&value).map(ParamOverrideValue::Float))
-        .or_else(|_| bool::from_str(&value).map(ParamOverrideValue::Bool))
-        .map_err(|_| anyhow!("must be one of i64, f64, or bool"))?;
-
-    Ok((key, value))
-}
-
-#[derive(clap::Subcommand, Debug, Clone)]
+#[derive(Subcommand, Debug, Clone)]
 enum Model {
     /// Use an already downloaded model
+    #[clap(name = "local")]
     Local {
-        /// The path to the model. e.g. `/home/marcus/.cache/huggingface/hub/models--TheBloke--Llama-2-7B-Chat-GGUF/blobs/08a5566d61d7cb6b420c3e4387a39e0078e1f2fe5f055f3a03887385304d4bfa`
+        /// The path to the model. e.g. `../hub/models--TheBloke--Llama-2-7B-Chat-GGUF/blobs/08a5566d61d7cb6b420c3e4387a39e0078e1f2fe5f055f3a03887385304d4bfa`
+        /// or `./llama-3.2-1b-instruct-q8_0.gguf`
         path: PathBuf,
     },
     /// Download a model from huggingface (or use a cached version)
@@ -117,136 +61,209 @@ impl Model {
     }
 }
 
-#[allow(clippy::too_many_lines)]
-fn main() -> Result<()> {
-    let Args {
-        n_len,
-        model,
-        prompt,
-        file,
-        #[cfg(any(feature = "cuda", feature = "vulkan"))]
-        disable_gpu,
-        key_value_overrides,
-        seed,
-        threads,
-        threads_batch,
-        ctx_size,
-    } = Args::parse();
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum Mode {
+    Chat,
+    Completion,
+}
+
+#[derive(Parser, Debug, Clone)]
+struct Args {
+    /// The path to the model
+    #[command(subcommand)]
+    model: Model,
+
+    /// The mode of the code: completion or chat
+    #[clap(value_enum, short = 'm', long, default_value = "chat")]
+    mode: Mode,
+
+    // /// The prompt to use - valid only if the mode is `completion`
+    // #[clap(short = 'p', long, required_if_eq("mode", "completion"))]
+    // prompt: Option<String>,
+
+    /// set the length of the prompt + output in tokens
+    #[clap(long, default_value_t = 512)]
+    max_token: i32,
+
+    /// override some parameters of the model
+    #[clap(short = 'o', value_parser = parse_key_val)]
+    key_value_overrides: Vec<(String, ParamOverrideValue)>,
+
+    /// how many layers to keep on the gpu - zero is cpu mode
+    #[clap(
+        short = 'g',
+        long,
+        help = "how many layers to keep on the gpu - zero is cpu mode (default: 0)"
+    )]
+    n_gpu_layers: u32,
+
+    /// set the seed for the RNG
+    #[clap(short = 's', long, default_value_t=561371, help = "RNG seed (default: 1234)")]
+    seed: u32,
+
+    /// number of threads to use during generation
+    #[clap(
+        long,
+        help = "number of threads to use during generation (default: use all available threads)"
+    )]
+    threads: Option<i32>,
+    #[clap(
+        long,
+        help = "number of threads to use during batch and prompt processing (default: use all available threads)"
+    )]
+    threads_batch: Option<i32>,
+
+    /// size of the prompt context
+    #[clap(
+        short = 'c',
+        long,
+        help = "size of the prompt context (default: loaded from the model)"
+    )]
+    ctx_size: Option<NonZeroU32>,
+
+}
+
+/// Parse a single key-value pair
+fn parse_key_val(s: &str) -> Result<(String, ParamOverrideValue)> {
+    let pos = s
+        .find('=')
+        .ok_or_else(|| anyhow!("invalid KEY=value: no `=` found in `{}`", s))?;
+    let key = s[..pos].parse()?;
+    let value: String = s[pos + 1..].parse()?;
+    let value = i64::from_str(&value)
+        .map(ParamOverrideValue::Int)
+        .or_else(|_| f64::from_str(&value).map(ParamOverrideValue::Float))
+        .or_else(|_| bool::from_str(&value).map(ParamOverrideValue::Bool))
+        .map_err(|_| anyhow!("must be one of i64, f64, or bool"))?;
+
+    Ok((key, value))
+}
+
+fn main() {
+
+    let args: Args = Args::parse();
 
     // init LLM
-    let backend = LlamaBackend::init()?;
+    let backend = LlamaBackend::init()
+        .expect("Could not initialize Llama backend");
 
     // offload all layers to the gpu
     let model_params = {
-        #[cfg(any(feature = "cuda", feature = "vulkan"))]
-        if !disable_gpu {
-            LlamaModelParams::default().with_n_gpu_layers(1000)
+        if args.n_gpu_layers > 0 {
+            LlamaModelParams::default().with_n_gpu_layers(args.n_gpu_layers)
         } else {
             LlamaModelParams::default()
         }
-        #[cfg(not(any(feature = "cuda", feature = "vulkan")))]
-        LlamaModelParams::default()
-    };
-
-    let prompt = if let Some(str) = prompt {
-        if file.is_some() {
-            bail!("either prompt or file must be specified, but not both")
-        }
-        str
-    } else if let Some(file) = file {
-        std::fs::read_to_string(&file).with_context(|| format!("unable to read {file}"))?
-    } else {
-        "Hello my name is".to_string()
     };
 
     let mut model_params = pin!(model_params);
-
-    for (k, v) in &key_value_overrides {
-        let k = CString::new(k.as_bytes()).with_context(|| format!("invalid key: {k}"))?;
+    
+    for (k, v) in &args.key_value_overrides {
+        let k = CString::new(k.as_bytes()).with_context(|| format!("invalid key: {k}")).unwrap();
         model_params.as_mut().append_kv_override(k.as_c_str(), *v);
     }
 
-    let model_path = model
+    let model_path = args.model
         .get_or_load()
-        .with_context(|| "failed to get model from args")?;
+        .with_context(|| "failed to get model from args").unwrap();
 
     let model = LlamaModel::load_from_file(&backend, model_path, &model_params)
-        .with_context(|| "unable to load model")?;
+        .with_context(|| "unable to load model").unwrap();
+
 
     // initialize the context
     let mut ctx_params = LlamaContextParams::default()
-        .with_n_ctx(ctx_size.or(Some(NonZeroU32::new(2048).unwrap())))
-        .with_seed(seed.unwrap_or(1234));
-    if let Some(threads) = threads {
+        .with_n_ctx(args.ctx_size.or(Some(NonZeroU32::new(2048).unwrap())))
+        .with_seed(args.seed);
+    if let Some(threads) = args.threads {
         ctx_params = ctx_params.with_n_threads(threads);
     }
-    if let Some(threads_batch) = threads_batch.or(threads) {
+    if let Some(threads_batch) = args.threads_batch.or(args.threads) {
         ctx_params = ctx_params.with_n_threads_batch(threads_batch);
     }
 
     let mut ctx = model
         .new_context(&backend, ctx_params)
-        .with_context(|| "unable to create the llama_context")?;
+        .with_context(|| "unable to create the llama_context").unwrap();
+    
+    // main loop
+    let mut input_to_model = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>You are a helpful assistant<|eot_id|>".to_string();
+    let mut input = String::new();
+    
+    print!("Assistant: How can I help you today?\n");
+    loop {
+        
+        input.clear();
+        println!("\nYou: ");
+        std::io::stdout().flush().unwrap();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        
+        if input.is_empty() {
+            break;
+        }
+        
+        
+        let input_formatted = format!("<|start_header_id|>user<|end_header_id|>{}<|eot_id|><|start_header_id|>assistant<|end_header_id|>", input);
+        
+        input_to_model.push_str(input_formatted.as_str());
+        
+        
 
-    // tokenize the prompt
+        // tokenize the prompt
+        let tokens_list = model
+            .str_to_token(&input_to_model, AddBos::Never)
+            .with_context(|| format!("failed to tokenize {input_to_model}")).unwrap();
 
-    let tokens_list = model
-        .str_to_token(&prompt, AddBos::Always)
-        .with_context(|| format!("failed to tokenize {prompt}"))?;
+        let n_cxt = ctx.n_ctx() as i32;
+        let n_kv_req = tokens_list.len() as i32 + (args.max_token - tokens_list.len() as i32);
+        // let max_token = args.max_token;
 
-    let n_cxt = ctx.n_ctx() as i32;
-    let n_kv_req = tokens_list.len() as i32 + (n_len - tokens_list.len() as i32);
+        // eprintln!("n_len = {max_token}, n_ctx = {n_cxt}, k_kv_req = {n_kv_req}");
 
-    eprintln!("n_len = {n_len}, n_ctx = {n_cxt}, k_kv_req = {n_kv_req}");
-
-    // make sure the KV cache is big enough to hold all the prompt and generated tokens
-    if n_kv_req > n_cxt {
-        bail!(
+        // make sure the KV cache is big enough to hold all the prompt and generated tokens
+        if n_kv_req > n_cxt {
+            panic!(
             "n_kv_req > n_ctx, the required kv cache size is not big enough
 either reduce n_len or increase n_ctx"
         )
-    }
+        }
 
-    if tokens_list.len() >= usize::try_from(n_len)? {
-        bail!("the prompt is too long, it has more tokens than n_len")
-    }
+        if tokens_list.len() >= usize::try_from(args.max_token).unwrap() {
+            panic!("the prompt is too long, it has more tokens than n_len")
+        }
+        
 
-    // print the prompt token-by-token
-    eprintln!();
+        // create a llama_batch with size ctx_size
+        
+        // we use this object to submit token data for decoding
+        let mut batch = LlamaBatch::new(n_cxt as usize, 1);
 
-    for token in &tokens_list {
-        eprint!("{}", model.token_to_str(*token, Special::Tokenize)?);
-    }
+        let last_index: i32 = (tokens_list.len() - 1) as i32;
+        for (i, token) in (0_i32..).zip(tokens_list.into_iter()) {
+            // llama_decode will output logits only for the last token of the prompt
+            let is_last = i == last_index;
+            batch.add(token, i, &[0], is_last).unwrap();
+        }
 
-    std::io::stderr().flush()?;
+        ctx.decode(&mut batch)
+            .with_context(|| "llama_decode() failed").unwrap();
 
-    // create a llama_batch with size 512
-    // we use this object to submit token data for decoding
-    let mut batch = LlamaBatch::new(512, 1);
+        // main loop
 
-    let last_index: i32 = (tokens_list.len() - 1) as i32;
-    for (i, token) in (0_i32..).zip(tokens_list.into_iter()) {
-        // llama_decode will output logits only for the last token of the prompt
-        let is_last = i == last_index;
-        batch.add(token, i, &[0], is_last)?;
-    }
+        let mut n_cur = batch.n_tokens();
+        let mut n_decode = 0;
 
-    ctx.decode(&mut batch)
-        .with_context(|| "llama_decode() failed")?;
+        // let t_main_start = ggml_time_us();
 
-    // main loop
-
-    let mut n_cur = batch.n_tokens();
-    let mut n_decode = 0;
-
-    let t_main_start = ggml_time_us();
-
-    // The `Decoder`
-    let mut decoder = encoding_rs::UTF_8.new_decoder();
-
-    while n_cur <= n_len {
-        // sample the next token
-        {
+        // The `Decoder`
+        let mut decoder = encoding_rs::UTF_8.new_decoder();
+        
+        let mut llm_output = String::new();
+        
+        while n_cur <= args.max_token {
+            // sample the next token
             let candidates = ctx.candidates();
 
             let candidates_p = LlamaTokenDataArray::from_iter(candidates, false);
@@ -256,42 +273,48 @@ either reduce n_len or increase n_ctx"
 
             // is it an end of stream?
             if model.is_eog_token(new_token_id) {
-                eprintln!();
+                // eprintln!();
                 break;
             }
 
-            let output_bytes = model.token_to_bytes(new_token_id, Special::Tokenize)?;
+            let output_bytes = model.token_to_bytes(new_token_id, Special::Tokenize).unwrap();
             // use `Decoder.decode_to_string()` to avoid the intermediate buffer
             let mut output_string = String::with_capacity(32);
             let _decode_result = decoder.decode_to_string(&output_bytes, &mut output_string, false);
+            
             print!("{output_string}");
-            std::io::stdout().flush()?;
+            llm_output.push_str(output_string.as_str());
+            std::io::stdout().flush().unwrap();
 
             batch.clear();
-            batch.add(new_token_id, n_cur, &[0], true)?;
+            batch.add(new_token_id, n_cur, &[0], true).unwrap();
+
+            n_cur += 1;
+
+            ctx.decode(&mut batch).with_context(|| "failed to eval").unwrap();
+
+            n_decode += 1;
         }
+        
+        let llm_output_formatted = format!("{}<|eot_id|>", llm_output);
+        
+        input_to_model.push_str(llm_output_formatted.as_str());
 
-        n_cur += 1;
-
-        ctx.decode(&mut batch).with_context(|| "failed to eval")?;
-
-        n_decode += 1;
+        // eprintln!("\n");
+        // 
+        // let t_main_end = ggml_time_us();
+        // 
+        // let duration = Duration::from_micros((t_main_end - t_main_start) as u64);
+        // 
+        // eprintln!(
+        //     "decoded {} tokens in {:.2} s, speed {:.2} t/s\n",
+        //     n_decode,
+        //     duration.as_secs_f32(),
+        //     n_decode as f32 / duration.as_secs_f32()
+        // );
+        // 
+        // println!("{}", ctx.timings());
+        
     }
 
-    eprintln!("\n");
-
-    let t_main_end = ggml_time_us();
-
-    let duration = Duration::from_micros((t_main_end - t_main_start) as u64);
-
-    eprintln!(
-        "decoded {} tokens in {:.2} s, speed {:.2} t/s\n",
-        n_decode,
-        duration.as_secs_f32(),
-        n_decode as f32 / duration.as_secs_f32()
-    );
-
-    println!("{}", ctx.timings());
-
-    Ok(())
 }
